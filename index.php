820c2a50a31ac40bd4465e7502ec56034f135b55
Sudeep Shrestha <sudipshrestha54321@gmail.com>
2026-08-19 08:20:36 +0545

    Php commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+echo "Hello World";
+echo "<h1>Welcome to my PHP page</h1>";
+
+?>
+
+<h1>I am a PHP</h1>    
+
+<?php
+
+echo "This is a PHP file that outputs HTML content.";
+
+?>
+
+
+<a href="registration.php">Go to Registration Page</a>
---
+++ registration.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Page</title>
+    <style>
+        /* Tag Selector */
+      p{
+          background-color: yellow;
+      }
+
+      #para1{
+   color: green;
+      }
+
+      .para2{
+        color: blue;
+      }
+
+    </style>
+</head>
+<body>
+    <h1>Registration Page</h1>
+    <!--  Inline css  -->
+    <p style="color:red">Welcome to registration page</p>
+     <p id="para1">This is a para 1</p>
+     <p class="para2">This is a para 2</p>
+     <p class="para2">This is a para 3</p>
+     <p>This is a para 4</p>
+</body>
+</html>
+
